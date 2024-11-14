d7c8203880b6bb6aac4d4673c2acee80eb403534
RzayevUmid0023 <umid.official.main@gmail.com>
2024-11-14 15:02:36 +0400

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'] // Bu gibi dışa bağımlılıkları doğru şekilde ayarlayın.
+    }
+  }
+});
